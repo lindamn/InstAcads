c6e74cce8e76879e26b6d29f0cc1c6c56f31c780
Kristine Labador <knlabador@up.edu.ph>
2021-05-23 22:22:08 +0800

    Update css file of ProfileEdit and change default pic image

--- js-files/Profile.js
+++ js-files/Profile.js
@@ -44,6 +44,36 @@ class Profile extends HTMLElement{
         return this.getAttribute('class')
     }
  
+    update() {
+        var name = document.getElementById('name');
+        var bio = document.getElementById('bio');
+        var univ = document.getElementById('univ');
+        var degree = document.getElementById('degree');
+        var year = document.getElementById('year');
+        var bday = document.getElementById('bday');
+
+        alert(input.value);
+        this.shadow.innerHTML = `
+            <link rel="stylesheet" href="../css/Profile.css">
+            <div id="main-container">
+                <div id="content-container">
+                    <p id="info1">name.value</p>
+                    <p id="info2">bio.value</p>
+                    <br/><br/>
+                    <p id="info3">${this.univ}</p>
+                    <p id="info3">${this.degree}</p>
+                    <p id="info3">${this.year} Level</p>
+                    <br/><br/>
+                    <p id="info3">Joined ${this.join}</p>
+                    <p id="info3">Born ${this.bday}</p>
+                    <br/><br/>
+                    <p id="info3">Friends ${this.friend}</p>
+                    <p id="info3">Classes ${this.class}</p>
+                </div>
+            </div>
+        `;
+    } 
+
     render(){
         this.shadow.innerHTML = `
             <link rel="stylesheet" href="../css/Profile.css">
@@ -54,7 +84,7 @@ class Profile extends HTMLElement{
                     <br/><br/>
                     <p id="info3">${this.univ}</p>
                     <p id="info3">${this.degree}</p>
-                    <p id="info3">${this.year}</p>
+                    <p id="info3">${this.year} Level</p>
                     <br/><br/>
                     <p id="info3">Joined ${this.join}</p>
                     <p id="info3">Born ${this.bday}</p>
