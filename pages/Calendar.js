42e838b41532d6f3e2f63ec35cbfcb22a43d0671
Lindsay Penaranda <60214729+lindamn@users.noreply.github.com>
2021-05-31 17:09:10 +0800

    Update Calendar.js

--- pages/Calendar.js
+++ pages/Calendar.js
@@ -11,7 +11,7 @@ class Calendar extends HTMLElement {
     <link rel="stylesheet" href="Calendar.css">
 <div class="jzdbox1 jzdbasf jzdcal">
 
-<div class="jzdcalt">June 2017</div>
+<div class="jzdcalt">MAY 2021</div>
 
 <span>Su</span>
 <span>Mo</span>
@@ -29,7 +29,7 @@ class Calendar extends HTMLElement {
 <span class="jzdb"><!--BLANK--></span>
 <span class="jzdb"><!--BLANK--></span>
 <span>1</span>
-<span class="circle" data-title="My 25th birthday!">2</span>
+<span class="circle" data-title="Unit 1, Fundamentals of Kemerut">2</span>
 <span>3</span>
 <span>4</span>
 <span>5</span>
@@ -38,8 +38,8 @@ class Calendar extends HTMLElement {
 <span>8</span>
 <span>9</span>
 <span>10</span>
-<span>11</span>
-<span class="circle" data-title="2 month anniversary!">12</span>
+<span class="circle" data-title="Sprint 143">11</span>
+<span>12</span>
 <span>13</span>
 <span>14</span>
 <span>15</span>
@@ -49,7 +49,7 @@ class Calendar extends HTMLElement {
 <span>19</span>
 <span>20</span>
 <span>21</span>
-<span class="circle" data-title="#MusicMonday - share your favorite song!">22</span>
+<span>22</span>
 <span>23</span>
 <span>24</span>
 <span>25</span>
@@ -58,6 +58,7 @@ class Calendar extends HTMLElement {
 <span>28</span>
 <span>29</span>
 <span>30</span>
+<span class="circle" data-title="Make a Prototype">31</span>
 <span class="jzdb"><!--BLANK--></span>
 <span class="jzdb"><!--BLANK--></span>
 <span class="jzdb"><!--BLANK--></span>
