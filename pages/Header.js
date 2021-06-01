4d16c14c84dd9d950bfeed594e99a56976454bfe
ckrp120 <kpcadacio1@up.edu.ph>
2021-06-01 14:33:09 +0800

    change kevin

--- pages/Header.js
+++ pages/Header.js
@@ -22,7 +22,7 @@ class Header extends HTMLElement{
                     <div class="topnav-right">
 
                          <! -- EDIT NAME -->
-                        <a href="Profile.html">MARK IGUIN</a>
+                        <a href="Profile.html">KEVIN GINES</a>
                         <a href="LoginPage.html">LOG OUT</a>
                     </div>
                 </div>
