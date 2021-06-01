f9a9439ff445d2864631ede430cf8fbfa8670d40
ckrp120 <kpcadacio1@up.edu.ph>
2021-06-01 17:46:08 +0800

    change kez

--- pages/Header.js
+++ pages/Header.js
@@ -22,7 +22,7 @@ class Header extends HTMLElement{
                     <div class="topnav-right">
 
                          <! -- EDIT NAME -->
-                        <a href="Profile.html">KUYUKOTU</a>
+                        <a href="Profile.html">KEZIA REYES</a>
                         <a href="LoginPage.html">LOG OUT</a>
                     </div>
                 </div>
